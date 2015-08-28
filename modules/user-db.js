6064b8186b362527dfb1f1286adfe1cc8d306c23
tinray <b03902072@ntu.edu.tw>
2015-08-28 18:59:55 +0800

    fix problem

--- modules/user-db.js
+++ modules/user-db.js
@@ -2,89 +2,104 @@ var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var dbConfig = require('../config/db.js');
 
-var init = function ( onConnected, onError ){
+var init = function ( onError, onConnect ){
     // Initialization
     dbConfig.urlurl = 'mongodb://localhost:27017';
     MongoClient.connect( dbConfig.url, function(err, db) {
         if( err !== null ){            
             return onError( err );
         }
-        console.log("Connected correctly to server.");            
-        return onConnected( db );
+        return onConnect( db );
     });
 };
 
+function handle( onSuccess, onError ){
+    return function(err, result){
+        if( ! err ){
+            onError( err );
+        }else{
+            onSuccess( result );
+        }
+    };
+};    
 
-module.export = {
+module.exports = {
 
     addUser: function( user, onSuccess, onError ){
-        function add( db ){
-            var result = db.collection('users').insert( user );
-            return onSuccess( result);
+        var onConnect = function( db ){
+            db.collection('users')
+                .insertOne( user, handle( onSuccess, onError) );
+            db.close();
         };
-        init( add, onError );
+        init( onError, onConnect );
     },
 
     deleteUserByFBId: function( fbId, onSuccess, onError ){
-        function del( db ){
-            var result = db.collection('users').remove( { "fb-id": fbId } );
-            return onSuccess( fbId );
+        var onConnect = function( db ){
+            db.collection('users')
+                .remove( { "fb-id": fbId }, handle( onSuccess, onError ) );
+            db.close();
         };
-        init( del, onError );
-    },
+        init( onError, onConnect );
+     },
 
     findUserByFBId: function( fbId, onSuccess, onError ){
-        function find( db ){
-            var result = db.collection('user').find( { "fb-id": fbId } );
-            return onSuccess( result );
+        var onConnect = function( db ){
+            db.collection('users')
+                .find( { "fb-id": fbId } )
+                .toArray( handle( onSuccess, onError ) );
+            db.close();
         };
-        init( find, onError );
+        init( onError, onConnect );
     },
-
             
     updateUserByFBId: function( fbId, user, onSuccess, onError ){
-        function update( db ){
-            var result = db.collection('user').
-                update( { "fb-id": fbId }, {$set: { user: user} } );
-            return onSuccess( result );
-        }
-        init( update, onError );
+        var onConnect = function( db ){
+            db.collection('users')
+                .update( { "fb-id": fbId }, { $set: user }, handle( onSuccess, onError ) );
+            db.close();
+        };
+        init( onError, onConnect );
     },
 
     findUser: function( property, value, onSuccess, onError ){
-        function find( db ){
-            var query = {};
-            query[property] = value;
-            var result = db.collection('user').find( query );
-            return onSuccess( result );
-        }
-        init( find, onError );
+        var query = {};
+        query[property] = value;
+        var onConnect = function( db ){
+            db.collection('users')
+                .find( query )
+                .toArray( handle( onSuccess, onError ) );
+        };
+        init( onError, onConnect );
     },
 
     updateUser: function( property, value, user, onSuccess, onError ){
-        function update( db ){
-            var query = {};
-            query[property] = value;
-            var result = db.collection('user').update( { query }, { $set: user });
-            return onSuccess( result );
-        }
-        init( update, onError );
+        var query = {};
+        query[property] = value;
+        var onConnect = function( db ){
+            db.collection('users')
+                .update( query, { $set: user }, handle( onSuccess, onError ) );
+            db.close( db );
+        };
+        init( onError, onConnect );
     },
 
     findUserByQuery: function( query, onSuccess, onError){
-        function find( db ){
-            var result = db.collection('user').find( query );
-            return onSuccess( result );
-        }
-        init( find, onError );
+        var onConnect = function( db ){
+            db.collection('users')
+                .find( query ).toArray( handle( onSuccess, onError ) );
+            db.close( db );
+        };
+        init( onError, onConnect );
     },
 
     updateUserByQuery: function( query, user, onSuccess, onError ){
-        function find( db ){
-            var result = db.collection('user').update( query, { $set: user } );
-            return onSuccess( result );
-        }
-        init( find, onError );
+        var onConnect = function( db ){
+            db.collection('users')
+                .update( query, { $set: user }, handle( onSuccess, onError ) );
+            db.close( db );
+        };
+        init( onError, onConnect );
     }
             
 };
