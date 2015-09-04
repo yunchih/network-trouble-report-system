317a504312fd5022038cd4c1c729d4491acf38d1
tinray <b03902072@ntu.edu.tw>
2015-09-04 18:28:26 +0800

    fix minor bug

--- middlewares/register.js
+++ middlewares/register.js
@@ -7,21 +7,23 @@ var checkRequest = require( '../modules/permission.js' ).request( config );
 
 
 router.post( '/', function( req, res, next ){
-    checkRequest( req, res );
+    if( !checkRequest( req, res ) ){
+        return false;
+    }
 
-    if( ! req.query.agree ){
+    if( req.query.agree !== "true" ){
         return res.json( {error: "User doesn't agree with term of service"} ); 
     }
 
     delete req.query.agree;
     
     var user = req.query;
-    user.fbId = req.user.fbId;
+    user.fbId = req.user.id;
     user.permission = "general";
 
     function added( result ){
         console.log( "Create a new user." );
-        req.session.fbId = user.fbId;
+        req.session.fbId = user.id;
         req.session.permission = user.permission;           
         res.send( { success: "true" } );
     };
