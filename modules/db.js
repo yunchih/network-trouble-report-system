025e13931470217eb46919a912b3e0af7a0c9f53
tinray <b03902072@ntu.edu.tw>
2015-09-04 22:14:58 +0800

    db module with shared connection

---
+++ modules/db.js
@@ -0,0 +1,27 @@
+var MongoClient = require('mongodb').MongoClient;
+var dbConfig = require('../config/db.js');
+    
+function handle( db, onSuccess, onError ){
+    return function(err, result){
+        db.close();
+        if( err !== null ){
+            onError( err );
+        }else{
+            onSuccess( result );
+        }
+    };
+};    
+
+module.exports = {
+    init: function( onConnect ){
+        // Initialization
+        MongoClient.connect( dbConfig.url, function(err, db) {
+            if( err !== null ){            
+                return console.log("Error connect to db!!");
+            }
+            console.log("Database connect successfully.");
+            return onConnect( db );
+        });
+    }
+};
+    
