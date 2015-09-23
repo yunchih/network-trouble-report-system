c995a397b016a045017224a7018694133562dad8
tinray <b03902072@ntu.edu.tw>
2015-09-23 22:55:44 +0800

    recaptcha add dev mode

--- modules/recaptcha.js
+++ modules/recaptcha.js
@@ -1,23 +1,25 @@
 var https = require('https');
 var config = require('../config/recaptcha.js');
+var request = require('request');
 
 module.exports = {
     verify: function( response, callback ){
-        https.get("https://www.google.com/recaptcha/api/siteverify?secret=" + config.secret +
-                  "&response=" + response,
-                  function(res) {
-                      var data = "";
-                      res.on('data', function (chunk) {
-                          data += chunk.toString();
-                      });
-                      res.on('end', function() {
-                          try {
-                              var parsedData = JSON.parse(data);                              
-                              callback(null, parsedData.success);
-                          } catch (e) {
-                              callback(e, false);
-                          }
-                      });
-                  });
+        try{
+            request.get("https://www.google.com/recaptcha/api/siteverify?secret=" + config.secret +
+                        "&response=" + response, function( err, status, data ){
+                            try {
+                                if( err !== null ){
+                                    return console.log( err );
+                                }
+                                var parsedData = JSON.parse(data);                              
+                                return callback(null, parsedData.success);
+                            } catch (e) {
+                                return callback(e, false);
+                            }
+                        });
+        }
+        catch( e ){
+            callback( e, false );
+        }
     }
 };
