4632d47449b68b29f64556dee88020ef950a1819
tinray <b03902072@ntu.edu.tw>
2015-09-23 18:48:10 +0800

    renew recaptcha.js

--- config/recaptcha.js
+++ config/recaptcha.js
@@ -1,3 +1,11 @@
-module.exports = {
-    secret: "6LfbdQwTAAAAAEDl7ZCqgzYhR2my59myT-oIIyY8"
-};
+
+if( process.env.DEV ){
+    console.log( "Warning: Recaptcha is in DEV mode !!" );
+    module.exports = {
+        secret: "6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe"
+    };
+}else{ 
+    module.exports = {        
+        secret: "6LfbdQwTAAAAAEDl7ZCqgzYhR2my59myT-oIIyY8"
+    };
+}   
